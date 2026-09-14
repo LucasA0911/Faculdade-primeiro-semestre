b9ab536170e8a3a11f494eee681dced643a3d405
LUCAS ALVES DOS SANTOS <lucas.asantos107@senacsp.edu.br>
2026-09-14 11:37:25 -0300

    Aula 06

---
+++ Algoritimos e Programação para IA/Exercicios/ex06_lucas_alves_dos_santos.py
@@ -0,0 +1,11 @@
+def questao(int:int):
+    print(f'''
+    ------------------------------------------
+                   Questão: {int}
+    ------------------------------------------''')
+
+lista = [1, 2, 3]
+
+dic = {'Manoel': 1.75, 'Leticia': 1.60, 'Taciana': 1.70}
+
+print(type(dic))
